3871a29892dd4f6e6555420b857cc9d5446e1433
Алексей А. Соин <soinaa@rubezh.ru>
2021-07-27 14:23:25 +0400

    add: custom input

---
+++ src/components/ZenInput.vue
@@ -0,0 +1,24 @@
+<template>
+    <label class="block">
+        <span v-if="label" class="text-gray-700">{{ label }}</span>
+        <input class="mt-1 block w-full rounded-md border-gray-300 focus:ring-0"
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
+            :placeholder="placeholder"
+            :type="typeNow"
+        >
+    </label>
+</template>
+<script>
+export default {
+    props: ['label', 'placeholder', 'modelValue', 'type-input'],
+    computed: {
+        typeNow() {
+            return this.typeInput || "text";
+        },
+    },
+    methods: {
+
+    },
+}
+</script>
